9573132fe2913589bfe130862b4be87b57b873e3
Daniil <dpancov64@gmail.com>
2022-06-27 17:28:32 +0300

    renamed and exported as default

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-const token = fs.readFileSync(
+const emailBody = fs.readFileSync(
   path.resolve(__dirname, './templates/token.html'),
   'utf8'
 ) as string
 
-export { token }
+export default emailBody
