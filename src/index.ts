372a73eae0d3f4d4dafdfcd3fed09c8e7e562523
smyzin <smyzin.work@gmail.com>
2022-06-28 00:25:50 +0300

    Add responsive and fix styles

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-const token = fs.readFileSync(
+const htmlBody = fs.readFileSync(
   path.resolve(__dirname, './templates/token.html'),
   'utf8'
 ) as string
 
-export { token }
+export { htmlBody }
