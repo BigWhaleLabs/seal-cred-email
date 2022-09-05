46483f9c74d49ea2dd57ca768d5617b03b50572d
smyzin <smyzin.work@gmail.com>
2022-09-05 21:41:47 +0300

    Changed address

--- src/env.ts
+++ src/env.ts
@@ -2,5 +2,5 @@ import { cleanEnv, str } from 'envalid'
 
 // eslint-disable-next-line node/no-process-env
 export default cleanEnv(process.env, {
-  SEALCRED_ADDRESS: str({ default: 'https://sealcred.com' }),
+  SEALCRED_ADDRESS: str({ default: 'https://sealcred.xyz' }),
 })
