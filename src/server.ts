ec5fbb472092df51e03dd0d1187aee35b4d1fedb
T_DaMER <dpancov64@gmail.com>
2023-04-17 16:46:39 +0300

    Use bwl-prettier and eslint configs

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import * as express from 'express'
-import * as token from './templates/token'
+import * as token from 'src/templates/token'
 
 const port = 3002
 const app = express()
 
 app.get('*', (req, res) => {
   const { html } = token.generateTokenHtml({
-    secret: '906090',
     domain: 'bwl.gg',
+    secret: '906090',
   })
   res.send(html)
 })
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,10 +1,10 @@
 export default {
   accent: '#fed823',
-  secondary: '#ff7bed',
-  tertiary: '#01feb6',
+  darkBlue: '#4B61D5',
   formal: '#efecd6',
+  primary: '#15a1fc',
   primaryBackground: '#1a0259',
   primaryDark: '#0d0030',
-  primary: '#15a1fc',
-  darkBlue: '#4B61D5',
+  secondary: '#ff7bed',
+  tertiary: '#01feb6',
 }
