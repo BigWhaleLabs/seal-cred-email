7ad1aec50ea2d3e15a2733d34329d6849af887ed
T_DaMER <dpancov64@gmail.com>
2023-06-09 16:56:52 +0300

    Use hash address

--- src/env.ts
+++ src/env.ts
@@ -2,5 +2,5 @@ import { cleanEnv, str } from 'envalid'
 
 // eslint-disable-next-line node/no-process-env
 export default cleanEnv(process.env, {
-  KETL_ADDRESS: str({ default: 'https://ketl.xyz' }),
+  KETL_ADDRESS: str({ default: 'https://ketl.xyz/#' }),
 })
